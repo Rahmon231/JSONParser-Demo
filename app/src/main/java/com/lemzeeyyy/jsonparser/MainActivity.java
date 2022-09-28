41a92b9fac4cab09530fe5e07d48aad594d52238
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-28 21:21:48 +0100

    initializing......

---
+++ app/src/main/java/com/lemzeeyyy/jsonparser/MainActivity.java
@@ -0,0 +1,60 @@
+package com.lemzeeyyy.jsonparser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    ArrayList<String> personNames = new ArrayList<>();
+    ArrayList<String> emailIds = new ArrayList<>();
+    ArrayList<String> mobileNumbers = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        try {
+            JSONObject json = new JSONObject("loadJSONFromAsset");
+            JSONArray array = json.getJSONArray("users");
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject userDetails = array.getJSONObject(i);
+                personNames.add(userDetails.getString("name"));
+                emailIds.add(userDetails.getString("email"));
+                JSONObject mobileObj = userDetails.getJSONObject("contact");
+                mobileNumbers.add(mobileObj.getString("mobile"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    private String loadJSONFromAsset(){
+        String json;
+        json = null;
+        try {
+            InputStream is = getAssets().open("users_list.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return json;
+    }
+}
